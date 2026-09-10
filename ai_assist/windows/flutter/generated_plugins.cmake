12416f8418213036983a6dc49e1c976b16212789
THANUSHRAJAN <thanushrajana@gmail.com>
2026-09-10 13:08:18 +0530

    new firebase updated

--- ai_assist/windows/flutter/generated_plugins.cmake
+++ ai_assist/windows/flutter/generated_plugins.cmake
@@ -3,7 +3,12 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  cloud_firestore
   file_selector_windows
+  firebase_auth
+  firebase_core
+  firebase_database
+  firebase_storage
   flutter_tts
   speech_to_text_windows
   url_launcher_windows
